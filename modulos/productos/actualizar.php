563ced1cce6adb5d46b55696623e8080129e5ae1
Helmunth Ávila <helmunth@gmail.com>
2026-03-08 19:12:00 -0500

    CRUD Productos - Visual

--- modulos/productos/actualizar.php
+++ modulos/productos/actualizar.php
@@ -1,25 +1,32 @@
 <?php
+// 1. Conexión a la base de datos
 include("../../conexion.php");
 
-$id = $_POST['id'];
-$nombre = $_POST['nombre'];
-$marca = $_POST['marca'];
-$talla = $_POST['talla'];
-$color = $_POST['color'];
-$precio = $_POST['precio'];
-$stock = $_POST['stock'];
+// 2. Recepción y limpieza de datos para seguridad
+$id       = $_POST['id'];
+$nombre   = mysqli_real_escape_string($conexion, $_POST['nombre']);
+$marca    = mysqli_real_escape_string($conexion, $_POST['marca']);
+$talla    = mysqli_real_escape_string($conexion, $_POST['talla']);
+$color    = mysqli_real_escape_string($conexion, $_POST['color']);
+$precio   = $_POST['precio'];
+$stock    = $_POST['stock'];
 
-$sql = "UPDATE productos SET
-nombre='$nombre',
-marca='$marca',
-talla='$talla',
-color='$color',
-precio='$precio',
-stock='$stock'
-WHERE id=$id";
+// 3. Sentencia SQL de actualización
+$sql = "UPDATE productos SET 
+        nombre = '$nombre', 
+        marca  = '$marca', 
+        talla  = '$talla', 
+        color  = '$color', 
+        precio = '$precio', 
+        stock  = '$stock' 
+        WHERE id = $id";
 
-mysqli_query($conexion,$sql);
-
-header("Location:listar.php");
-
+// 4. Ejecución y manejo de redirección
+if (mysqli_query($conexion, $sql)) {
+    // Regresa al listado con una señal de éxito
+    header("Location: listar.php?msj=actualizado");
+} else {
+    // Mensaje en caso de error técnico
+    echo "Error al actualizar el producto: " . mysqli_error($conexion);
+}
 ?>
